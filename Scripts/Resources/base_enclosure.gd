cec0107d6d14cd5b1410a1510b997cbb751ace91
verstraate <wilsjw2@student.op.ac.nz>
2025-07-29 23:44:20 +1200

    feat: Created enclosures with swipe navigation

---
+++ Scripts/Resources/base_enclosure.gd
@@ -0,0 +1,6 @@
+class_name BaseEnclosure
+extends Resource
+
+@export var type: Utils.MONSTER_TYPES = Utils.MONSTER_TYPES.Neutral
+@export var max_capacity: int = 10
+@export var background_sprite: Texture2D
---
+++ _Scenes/UI/enclosure_manager.gd
@@ -0,0 +1,71 @@
+class_name EnclosureManager
+extends Control
+
+const ENCLOSURE_SCENE = preload("res://_Scenes/Enclosure/enclosure.tscn")
+var _tween: Tween
+
+@export var test_enclosure: BaseEnclosure
+var enclosures: Array[Enclosure] = []
+var selected_enclosure: int = 0
+
+@export_group("Swipe Settings")
+@export_range(100, 500) var swipe_threshold: int
+@export_range(0, 1) var swipe_duration: float
+var swipe_dir: int = 0
+
+var swiping: bool = false
+var first_swipe_position: Vector2
+var curr_swipe_position: Vector2
+
+func _ready() -> void:
+	_add_enclosure()
+	_add_enclosure(test_enclosure)
+	_add_enclosure()
+
+func _process(delta: float) -> void:
+	if Input.is_action_just_pressed("press") and not swiping:
+		var mouse_pos = get_global_mouse_position()
+		swiping = get_rect().has_point(mouse_pos)
+		if swiping:
+			first_swipe_position = mouse_pos
+			curr_swipe_position = first_swipe_position
+		
+	if Input.is_action_pressed("press") and swiping:
+		curr_swipe_position = get_global_mouse_position()
+		if first_swipe_position.distance_to(curr_swipe_position) >= swipe_threshold:
+			var swipe_angle: float = abs(Vector2(first_swipe_position - curr_swipe_position).angle())
+			swipe_dir = 1 if swipe_angle > PI / 2 else -1
+			_handle_swipe()
+			swiping = false
+				
+	if Input.is_action_just_released("press") and swiping:
+		swiping = false
+		
+func _add_enclosure(new_enclosure: BaseEnclosure = null) -> void:
+	var pos_mod: int = 0 if enclosures.size() == selected_enclosure else 1
+	var enclosure: Enclosure = ENCLOSURE_SCENE.instantiate()
+	add_child(enclosure)
+	enclosure.setup_enclosure(new_enclosure)
+	enclosure.position = Vector2(pos_mod * enclosure.size.x, enclosure.position.y)
+	enclosures.append(enclosure)
+		
+func _handle_swipe() -> void:
+	if _tween != null and _tween.is_running():
+		return
+	
+	if (selected_enclosure - 1 < 0 and swipe_dir > 0) or (selected_enclosure + 1 > enclosures.size() - 1 and swipe_dir < 0):
+		return
+	
+	var next_enclosure: int = selected_enclosure - swipe_dir
+	var target_enclosure: Enclosure = enclosures[next_enclosure]
+	target_enclosure.position = Vector2(target_enclosure.size.x * -swipe_dir, target_enclosure.position.y)
+	
+	var curr_enclosure: Enclosure = enclosures[selected_enclosure]
+	var curr_enclosure_target: Vector2 = Vector2(curr_enclosure.size.x * swipe_dir, curr_enclosure.position.y)
+	
+	_tween = create_tween()
+	_tween.set_parallel(true)
+	_tween.tween_property(target_enclosure, "position", Vector2.ZERO, swipe_duration)
+	_tween.tween_property(curr_enclosure, "position", curr_enclosure_target, swipe_duration)
+	
+	selected_enclosure = next_enclosure
