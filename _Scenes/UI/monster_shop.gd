4a2374cfbdeb46ecf3ed2cd9ca72a5445fa639ab
verstraate <wilsjw2@student.op.ac.nz>
2025-07-31 00:45:15 +1200

    fix: Updated monsters_for_sale resource references for mobile exports

--- _Scenes/UI/monster_shop.gd
+++ _Scenes/UI/monster_shop.gd
@@ -3,12 +3,13 @@ extends Panel
 
 @onready
 var monster_parent: BoxContainer = $Monsters
-const SHOP_IEM = preload("res://_Scenes/ShopItem/shop_item.tscn")
+const SHOP_ITEM = preload("res://_Scenes/ShopItem/shop_item.tscn")
 
 @export var monsters_for_sale: Array[BaseMonster] = []
 signal shop_updated(monsters_changed: Array[BaseMonster])
 
 func _ready() -> void:
+	print('shop item count: %d' % monsters_for_sale.size())
 	shop_updated.connect(_display_monsters)	
 	_display_monsters(monsters_for_sale)
 
@@ -17,8 +18,11 @@ func add_monsters_to_shop(new_monsters: Array[BaseMonster]) -> void:
 	shop_updated.emit(new_monsters)
 
 func _display_monsters(monsters_changed: Array[BaseMonster]) -> void:
+	print('test1')
 	if monster_parent.get_child_count() != monsters_for_sale.size():
+		print('test2')
 		for monster in monsters_changed:
-			var monster_shop_item: ShopItem = SHOP_IEM.instantiate()
+			print('monster added')
+			var monster_shop_item: ShopItem = SHOP_ITEM.instantiate()
 			monster_parent.add_child(monster_shop_item)
 			monster_shop_item.setup_item(monster)
