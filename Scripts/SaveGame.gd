c5da25dd01cc35d6842ad5282ffeb49a09eb245f
verstraate <wilsjw2@student.op.ac.nz>
2025-08-04 21:08:18 +1200

    feat: Added progressive unlocks to save

--- Scripts/SaveGame.gd
+++ Scripts/SaveGame.gd
@@ -8,6 +8,7 @@ var keys_to_ignore: Array[String] = [
 	"pos_y",
 	"scale",
 	"_save_time",
+	"currency_per_tick",
 	"price"
 ]
 
@@ -70,8 +71,24 @@ func load_game() -> void:
 			if keys_to_ignore.has(i):
 				continue
 			
-			if i == "_money" or i == "enclosure_cost":
+			if i == "_money":
+				new_object = new_object as MoneyManager
 				_setup_idle_number(new_object, i, node_data)
+				
+				var save_time: Dictionary = node_data["_save_time"]
+				var curr_time: Dictionary = Time.get_datetime_dict_from_system()
+				
+				var currency_per_tick: String = node_data["currency_per_tick"]
+				var afk_earnings: IdleNumber = _generate_afk_earnings(save_time, curr_time, currency_per_tick)
+				new_object.adjust_money(afk_earnings.array_to_num())
+				continue
+			
+			if i == "enclosure_cost":
+				_setup_idle_number(new_object, i, node_data)
+				continue
+			
+			if i == "locked_monsters":
+				_setup_resource_array(new_object, i, node_data)
 				continue
 			
 			if i == "monsters":
@@ -83,16 +100,7 @@ func load_game() -> void:
 				continue
 			
 			if i == "monsters_for_sale":
-				new_object = new_object as MonsterShop
-				var monsters_for_sale: Array[BaseMonster] = []
-				for monster in node_data[i]:
-					monsters_for_sale.append(load(node_data[i][monster]["data"]))
-				new_object.clear_monsters()
-				new_object.add_monsters_to_shop(monsters_for_sale)
-				for item in node_data[i]:
-					var curr_item: ShopItem = new_object.monsters_displayed[item]
-					curr_item.price = IdleNumber.new(node_data[i][item]["price"])
-					curr_item.price_label.text = "$%s" % curr_item.price.display_value(2)
+				_setup_monster_shop(new_object, i, node_data)
 				continue
 			
 			new_object.set(i, node_data[i])
@@ -102,6 +110,12 @@ func load_game() -> void:
 func _setup_idle_number(new_object, i: String, node_data: Dictionary) -> void:
 	var num: IdleNumber = IdleNumber.new(node_data[i])
 	new_object.set(i, num)
+
+func _setup_resource_array(new_object, i: String, node_data: Dictionary) -> void:
+	var array_to_set: Array
+	for res in node_data[i]:
+		array_to_set.append(load(res))
+	new_object.set(i, array_to_set)
 
 func _setup_monsters(monsters: Array, new_object: Enclosure) -> void:
 	var monsters_to_add: Array[Monster] = []
@@ -114,3 +128,59 @@ func _setup_monsters(monsters: Array, new_object: Enclosure) -> void:
 		monsters_to_add.append(new_monster)
 	
 	new_object.monsters = monsters_to_add
+
+func _setup_monster_shop(new_object: MonsterShop, i: String, node_data: Dictionary) -> void:
+	var monsters_for_sale: Array[BaseMonster] = []
+	for monster in node_data[i]:
+		monsters_for_sale.append(load(node_data[i][monster]["data"]))
+	new_object.set_monsters_for_sale(monsters_for_sale)
+	for item in node_data[i]:
+		if not new_object.monsters_displayed.has(item):
+			continue
+		var curr_item: ShopItem = new_object.monsters_displayed[item]
+		curr_item.price = IdleNumber.new(node_data[i][item]["price"])
+		curr_item.price_label.text = "$%s" % curr_item.price.display_value(2)
+
+func _generate_afk_earnings(save_time: Dictionary, curr_time: Dictionary, currency_per_tick: String) -> IdleNumber:
+	var currency: IdleNumber = IdleNumber.new(currency_per_tick) # Currency generated per tick (0.1s)
+	
+	# GENERATE TIME AMOUNT CONSTANTS
+	currency.multiply(10) # 0.1s -> 1s
+	var second_total: IdleNumber = IdleNumber.new(currency.array_to_num())
+	currency.multiply(60) # 1s -> 60s -> 1m
+	var minute_total: IdleNumber = IdleNumber.new(currency.array_to_num())
+	currency.multiply(60) # 1m -> 60m -> 1hr
+	var hour_total: IdleNumber = IdleNumber.new(currency.array_to_num())
+	currency.multiply(24) # 1hr -> 24hr -> 1 day
+	var day_total: IdleNumber = IdleNumber.new(currency.array_to_num())
+	currency.multiply(30) # 1 day -> 30 days -> 1 month, close enough
+	var month_total: IdleNumber = IdleNumber.new(currency.array_to_num())
+	currency.multiply(12) # 1 month -> 12 months -> 1 year (360 days)
+	var year_total: IdleNumber = IdleNumber.new(currency.array_to_num())
+	
+	# DIFFERENCE BETWEEN save_time AND curr_time
+	var seconds: int = max(0, curr_time["second"] - save_time["second"])
+	var minutes: int = max(0, curr_time["minute"] - save_time["minute"])
+	var hours: int = max(0, curr_time["hour"] - save_time["hour"])
+	var days: int = max(0, curr_time["day"] - save_time["day"])
+	var months: int = max(0, curr_time["month"] - save_time["month"])
+	var years: int = max(0, curr_time["year"] - save_time["year"])
+	
+	# CALCULATE EARNINGS
+	
+	second_total.multiply(seconds)
+	minute_total.multiply(minutes)
+	hour_total.multiply(hours)
+	day_total.multiply(days)
+	month_total.multiply(months)
+	year_total.multiply(years)
+	
+	var total_earnings: IdleNumber = IdleNumber.new()
+	total_earnings.add(second_total.array_to_num())
+	total_earnings.add(minute_total.array_to_num())
+	total_earnings.add(hour_total.array_to_num())
+	total_earnings.add(day_total.array_to_num())
+	total_earnings.add(month_total.array_to_num())
+	total_earnings.add(year_total.array_to_num())
+	
+	return total_earnings
