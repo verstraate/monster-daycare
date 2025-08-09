8324cae0fe1354131a900a05bf66b22b7c885eb7
verstraate <wilsjw2@student.op.ac.nz>
2025-08-09 13:45:41 +1200

    feat: Created type preference table

--- Scripts/Utils.gd
+++ Scripts/Utils.gd
@@ -37,11 +37,19 @@ enum NUMBER_SUFFIXES {
 }
 
 enum MONSTER_TYPES {
-	Neutral,
-	Fire,
-	Water,
-	Earth,
-	Air,
+	Neutral = 0,
+	Fire = 1,
+	Water = 2,
+	Earth = 3,
+	Air = 4,
+}
+
+const TYPE_PREFERENCES: Dictionary[MONSTER_TYPES, Array] = {
+	MONSTER_TYPES.Neutral: [ 0,  0,  0,  0,  0],
+	MONSTER_TYPES.Fire:    [ 0,  1, -1,  0, -1],
+	MONSTER_TYPES.Water:   [ 0, -1,  1, -1,  0],
+	MONSTER_TYPES.Earth:   [ 0,  0, -1,  1, -1],
+	MONSTER_TYPES.Air:     [ 0, -1,  0, -1,  1],
 }
 
 var rng: RandomNumberGenerator = RandomNumberGenerator.new()
