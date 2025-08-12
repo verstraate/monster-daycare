31f3162a2c8d04d03e3d45b3c2ed8d1fb5ece64a
verstraate <wilsjw2@student.op.ac.nz>
2025-08-12 22:15:14 +1200

    fix: Adjusted monster option centring on enclosure capacity

--- _Scenes/TrainMonsterOption/train_monster_option.gd
+++ _Scenes/TrainMonsterOption/train_monster_option.gd
@@ -3,11 +3,9 @@ class_name TrainMonsterOption extends Button
 @onready
 var _info: VBoxContainer = $Info
 @onready
-var _name_label: Label = $Info/Name
+var _details_label: Label = $Info/Details
 @onready
 var _icon: TextureRect = $Info/Icon
-@onready
-var _level_label: Label = $Info/Level
 @onready
 var _cost_label: Label = $Info/Cost
 
@@ -22,24 +20,28 @@ func setup(new_monster: Monster, index: int, display_length: int) -> void:
 	var is_max_level: bool = _monster.level >= _monster.monster_data.max_level
 	disabled = is_max_level
 	
-	_name_label.text = _monster.monster_data.display_name
+	_details_label.text = "%s\nLevel: %d" % [_monster.monster_data.display_name, _monster.level]
 	_icon.texture = _monster.monster_data.sprite
-	_level_label.text = "Level: %d" % _monster.level
 	_cost_label.text = "$%s" % _monster.training_cost.display_value(2) if not is_max_level else ""
 	
 	var new_pos: Vector2 = Vector2.ZERO
 	
-	var x_mult: float = 0
-	var y_mult: float = -0.5
-	if display_length > 3:
-		if index < 3:
-			x_mult = float(index - 2) + 0.5
-			y_mult += -0.525
+	var max_capacity: int = Globals.enclosure_manager.enclosures[Globals.enclosure_manager.selected_enclosure].active_enclosure.max_capacity
+	var new_line_threshold: int = ceili(float(max_capacity) / 2)
+	var x_mult: float
+	var y_mult: float = -0.5 # Centre by default
+	var y_centre_offset: float = 0.55
+	var x_centre_offset: float = 0.5
+	
+	if display_length > new_line_threshold:
+		if index < new_line_threshold:
+			x_mult = float(index - (new_line_threshold - 1)) + x_centre_offset
+			y_mult -= y_centre_offset
 		else:
-			x_mult = float(index % 3) - 0.5 * (display_length % 3)
-			y_mult += 0.525
+			x_mult = float(index % new_line_threshold) - x_centre_offset * (display_length % new_line_threshold)
+			y_mult += y_centre_offset
 	else:
-		x_mult = float(index) + display_length * -0.5
+		x_mult = float(index) + display_length * -x_centre_offset
 	
 	new_pos.x += x_mult * size.x
 	new_pos.y += y_mult * size.y
